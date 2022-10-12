7066def335bb50730848ac51d5760552f25fb72a
Mikalay Hancharonok <classic76@tut.by>
2022-10-12 14:10:30 +0300

    feat: add task count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,24 +1,34 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given matrix where you have to find cats by ears "^^"
  *
- * @param {Array<Array>} matrix 
+ * @param {Array<Array>} matrix
  * @return {Number} count of cats found
  *
- * @example
- * countCats([
- *  [0, 1, '^^'],
- *  [0, '^^', 2],
- *  ['^^', 1, 2]
- * ]) => 3`
- *
- */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+ * @example */
+// console.log(
+//   countCats([
+//     [0, 1, "^^"],
+//     [0, "^^", 2],
+//     ["^^", 1, 2],
+//   ])
+// );
+
+// => 3`
+//*
+//*/
+function countCats(matrix) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let cats = 0;
+  for (lines of matrix) {
+    let cat = lines.filter((x) => x === "^^").length;
+    cats += cat;
+  }
+  return cats;
 }
 
 module.exports = {
-  countCats
+  countCats,
 };
