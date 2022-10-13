318ee3a45bdd135a553e35651692d7e7de5ae76c
Mikalay Hancharonok <classic76@tut.by>
2022-10-13 21:57:56 +0300

    feat: add minesweeper solution

--- src/test.js
+++ src/test.js
@@ -1,8 +1,40 @@
-function isMAC48Address(n) {
+function minesweeper(matrix) {
   //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
-  let regexp = /^[0-9a-f]{2}(-[0-9a-f]{2}){5}$/i;
-  return regexp.test(n);
+  const results = [];
+  for (let i = 0; i < matrix.length; i++) {
+    let resultX = [];
+    for (let j = 0; j < matrix[i].length; j++) {
+      let sum = 0;
+      for (let di = -1; di < 2; di++) {
+        let x = i + di;
+        for (let dj = -1; dj < 2; dj++) {
+          let y = j + dj;
+          if (
+            !(x === i && y === j) &&
+            x >= 0 &&
+            x < matrix.length &&
+            y >= 0 &&
+            y < matrix[i].length
+          )
+            sum += matrix[x][y] ? 1 : 0;
+        }
+      }
+      resultX.push(sum);
+    }
+    results.push(resultX);
+  }
+  return results;
 }
-
-console.log(isMAC48Address("00-1B-63-84-45-E6"));
+// const matrix = [
+//   [true, false, false],
+//   [false, true, false],
+//   [false, false, false],
+// ];
+console.log(
+  minesweeper([
+    [true, false, false],
+    [false, true, false],
+    [false, false, false],
+  ])
+);
