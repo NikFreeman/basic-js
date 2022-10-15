b90fafe4ec1b99d99263e99a59b6c11f5ad241b3
Nikolay Hancharonok <classic76@tut.by>
2022-10-15 12:06:07 +0300

    feat: transform array solution

--- src/test.js
+++ src/test.js
@@ -1,8 +1,43 @@
-function isMAC48Address(n) {
-  //throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-  let regexp = /^[0-9a-f]{2}(-[0-9a-f]{2}){5}$/i;
-  return regexp.test(n);
+class VigenereCipheringMachine {
+  constructor(direct = true) {
+    console.debug("constructor");
+    this._direct = direct;
+    this._alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    this._errorMsg = "Incorrect arguments!";
+  }
+
+  _converter(message, keyword, encrypt = true) {
+    if (!message || !keyword) {
+      throw new Error(this._errorMsg);
+    } else {
+      const msg = message.toUpperCase().split("");
+      const key = keyword
+        .repeat(Math.ceil(message.length / keyword.length))
+        .toUpperCase();
+
+      let i = 0;
+
+      const newMsg = msg.reduce((resultMsg, currentLetter) => {
+        if (this._alpha.includes(currentLetter)) {
+          const letterIndexInAlpha = this._alpha.indexOf(key[i++]);
+          const newAlpha = `${this._alpha.slice(
+            letterIndexInAlpha
+          )}${this._alpha.slice(0, letterIndexInAlpha)}`;
+          return encrypt
+            ? `${resultMsg}${newAlpha[this._alpha.indexOf(currentLetter)]}`
+            : `${resultMsg}${this._alpha[newAlpha.indexOf(currentLetter)]}`;
+        } else {
+          return `${resultMsg}${currentLetter}`;
+        }
+      }, "");
+
+      return this.direct ? newMsg : newMsg.split("").reverse().join("");
+    }
+  }
+
+  encrypt = (message, keyword) => this._converter(message, keyword);
+  decrypt = (message, keyword) => this._converter(message, keyword, false);
 }
 
-console.log(isMAC48Address("00-1B-63-84-45-E6"));
+const reverseMachine = new VigenereCipheringMachine(false);
+console.log(reverseMachine._direct);
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,23 +1,49 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if (!Array.isArray(arr))
+    throw new Error(`'arr' parameter must be an instance of the Array!`);
+  let resultArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case "--discard-next":
+        i++;
+        break;
+      case `--discard-prev`:
+        if (arr[i - 1] !== undefined && arr[i - 2] !== "--discard-next")
+          resultArray.pop();
+
+        break;
+      case `--double-next`:
+        if (arr[i + 1] !== undefined) resultArray.push(arr[i + 1]);
+        break;
+      case `--double-prev`:
+        if (arr[i - 1] !== undefined && arr[i - 2] !== "--discard-next")
+          resultArray.push(arr[i - 1]);
+        break;
+      default:
+        resultArray.push(arr[i]);
+        break;
+    }
+  }
+  return resultArray;
 }
 
 module.exports = {
-  transform
+  transform,
 };
